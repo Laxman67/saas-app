3e40f7b198680af0feb503549917ef4ec1c5e810
Laxman67 <laxmanhenricks94@gmail.com>
2025-06-26 00:58:27 +0530

    Implement companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
+import CompanionCard from '@/components/CompanionCard';
+import CompanionList from '@/components/CompanionList';
+import CTA from '@/components/CTA';
 import { Button } from '@/components/ui/button';
+import { Section } from 'lucide-react';
 import React from 'react';
 
 const Page = () => {
   return (
     <>
-      <h1 className="text-2xl underline">Welcome to saas app</h1>
+      <h1 className="text-2xl underline my-3">Popular Companions</h1>
 
-      <Button>Let's get Started </Button>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="The Flower of Sand"
+          topic="Neutral Energy"
+          subject="Literature"
+          duration={45}
+          color="#ffda56"
+        />
+
+        <CompanionCard
+          id="456"
+          name="The Edge of Time"
+          topic="Quantum Mysteries"
+          subject="Physics"
+          duration={60}
+          color="#7ed6df"
+        />
+
+        <CompanionCard
+          id="789"
+          name="Journey to the Core"
+          topic="Human Anatomy"
+          subject="Biology"
+          duration={30}
+          color="#ff6b81"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
     </>
   );
 };
