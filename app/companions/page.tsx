34dece0467d5168802a8d8a67f358177b9edbaa7
Laxman67 <laxmanhenricks94@gmail.com>
2025-06-30 23:55:04 +0530

    getAllCompanions

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { getAllCompanions } from '@/lib/actions/companions-action';
 
-const Page = () => {
+const CompanionLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  const companions = await getAllCompanions({ topic, subject });
+  console.log(companions);
+
   return <div>Companion Library</div>;
 };
 
-export default Page;
+export default CompanionLibrary;
--- lib/actions/companions-action.ts
+++ lib/actions/companions-action.ts
@@ -25,3 +25,30 @@ export const createCompanions = async (formData: CreateCompanion) => {
 
   return data[0]; // ✅ Return the created companion for use in client redirect
 };
+
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+  let query = supabase.from('Companions').select();
+  console.log('Query ,', query);
+  if (subject && topic) {
+    query = query
+      .ilike('subject', `%${subject}`)
+      .or(`topic.ilike.%${topic}%,nba`);
+  } else if (subject) {
+    query = query.ilike('subject', `%${subject}`);
+  } else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%,username.ilike.%${topic}%`);
+  }
+
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const { data: companions, error } = await query;
+  if (error) throw new Error(error.message);
+
+  return companions;
+};
