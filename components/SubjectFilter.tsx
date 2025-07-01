cf8a511c4e0bf022cb63761e86a42ef941cfb4da
Laxman67 <laxmanhenricks94@gmail.com>
2025-07-02 00:10:55 +0530

    Search and Subject Filter

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,5 +1,63 @@
+'use client';
+import { subjects } from '@/constants';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+import { useEffect, useState } from 'react';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
+
 const SubjectFilter = () => {
-  return <div>SubjectFilter</div>;
+  const [subject, setSubject] = useState('');
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get('subject');
+
+  useEffect(() => {
+    const newUrl = '';
+
+    if (subject) {
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'subject',
+        value: subject,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      if (pathname === '/companions') {
+        const newUrl = removeKeysFromUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['subject'],
+        });
+
+        router.push(newUrl, { scroll: false });
+      }
+    }
+  }, [subject]);
+
+  return (
+    <div className="border border-black rounded-lg outline-none">
+      <Select onValueChange={setSubject} defaultValue={subject}>
+        <SelectTrigger className="w-full outline-none border-none rounded-lg ">
+          <SelectValue placeholder="Select the subject" />
+        </SelectTrigger>
+
+        <SelectContent>
+          {subjects.map((subject) => (
+            <SelectItem key={subject} value={subject} className="capitalize">
+              {subject}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default SubjectFilter;
